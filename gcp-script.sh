c2958d08af47345a083cd857d9d540ddbed94573
babita0810 <b.kumari@applicaster.com>
2024-03-11 11:18:02 -0500

    updated

--- gcp-script.sh
+++ gcp-script.sh
@@ -5,14 +5,14 @@ CENTRALIZED_PROJECT_ID="centralized-log-monitoring"
 
 setup_log_sink(){
     source_project=$1
-    sink_name="logs-sink"
+    sink_name="log-sink"
     # local service_account="$3"
     echo "Setting up Log Sink in Source Project: $source_project"
 
         if ! gcloud logging sinks describe "$sink_name" --project="$source_project" &>/dev/null; then
-
-            gcloud config set project "$source_project"
             
+            gcloud config set project "$source_project"
+            export CLOUDSDK_CORE_PROJECT=$source_project
             echo "Creating log sink for "$source_project" project"
             gcloud logging sinks create "$sink_name" "logging.googleapis.com/projects/$CENTRALIZED_PROJECT_ID/locations/global/buckets/centralized-logs" 
             service_account=$(gcloud logging sinks describe "$sink_name" --project="$source_project" --format="value(writerIdentity)" | cut -d':' -f2-)
